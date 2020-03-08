1435c217c253dc859bc009d784998eaedb080806
richrosenthal <rrosenthal2013@gmail.com>
2020-03-07 19:03:48 -0500

    created table migration

--- db/migrate/20200307234755_create_gages.rb
+++ db/migrate/20200307234755_create_gages.rb
@@ -1,4 +1,10 @@
 class CreateGages < ActiveRecord::Migration
   def change
+    create_table :gages do |t|
+      t.string :name
+      t.date :date_created
+      t.date :due_date
+      t.date :previous_due_date
+    end
   end
 end
