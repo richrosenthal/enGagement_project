d1060cc5305b29afb58826462ac55cb5559a5929
richrosenthal <rrosenthal2013@gmail.com>
2020-03-11 21:07:02 -0400

    Added dummy data to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,19 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  get '/registrations/signup' do
+    erb :'/registrations/signup'
+  end
+
+  post '/registrations' do
+   @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+   @user.save
+   session[:user_id] = @user.id
+   puts params
+
+   redirect '/users/home'
+ end
+
   get "/index" do
     erb :index
   end
