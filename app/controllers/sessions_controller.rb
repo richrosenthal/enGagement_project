2bbb628911e5faf07314b5369f622240dd1551c2
richrosenthal <rrosenthal2013@gmail.com>
2020-03-22 11:58:06 -0400

    added get/login, post/sessions, get/logout routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,16 @@ class SessionsController < ApplicationController
   get '/login' do
     erb :'/sessions/login'
   end
-end 
+
+  post '/sessions' do
+    #login a user with this email and Password
+    login(params[:email], params[:password])
+    redirect '/home'
+  end
+
+  get '/logout' do
+    logout!
+    redirect '/index.erb'
+  end
+  
+end
