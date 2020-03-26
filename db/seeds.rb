7b48edd727de0d5368f15c02aa24feec99e21272
richrosenthal <rrosenthal2013@gmail.com>
2020-03-25 21:54:39 -0400

    add dummy data

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+
+#dummy users
+
+ricky = User.create(name: "Richard", password_digest: "itsasecrectoallofus",
+email: "rickyunddu@hotmail.com")
+
+emma = User.create(name: "Emma Rosenthal", password_digest: "memesrus",
+email: "cowsgoclickclick@hotmail.com")
+
+#dummy gages
+
+Gage.create (name: "Caliper", date_created: 12-10-19, due_date: 12-10-20,
+  previous_due_date: 12-10-10, user_id: ricky.id)
+
+Gage.create (name: "Caliper2", date_created: 12-11-19, due_date: 12-11-20,
+    previous_due_date: 12-11-10, user_id: emma.id)
