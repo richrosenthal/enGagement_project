2172e74a75d2b757837fc019276a1d9f43282341
richrosenthal <rrosenthal2013@gmail.com>
2020-03-26 22:01:16 -0400

    creates get route for new gages

--- app/controllers/gages_controller.rb
+++ app/controllers/gages_controller.rb
@@ -1,11 +1,13 @@
 class GagesController < ApplicationController
 
   #get gage_entries/new to render a form to create new entry
-
+  get '/gages/new' do
+    erb :'gages/new'
+  end
   # post gage_entries to create a new gage entry
 
-  # showo route for a gage entry
+  # show route for a gage entry
 
   #index route for all gage entries
-  
+
 end
