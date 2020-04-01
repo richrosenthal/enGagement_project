743d99a5bff2d9cbd7f195693878ee639706a5bd
richrosenthal <rrosenthal2013@gmail.com>
2020-04-01 12:15:59 -0400

    validate presence of name, email, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_many :gages
-  has_secure_password 
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
   validates :email, uniqueness: true
 
 end
